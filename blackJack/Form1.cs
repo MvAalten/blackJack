3752b96ad0ebc6e53350c876603fa6b409456889
MvAalten <157478216+MvAalten@users.noreply.github.com>
2025-03-18 13:29:58 +0100

    Created a shuffle function which swaps every card with another random card. For example, if 1 and 2 are chosen, it'll go 2 1 3 4 5, etc., until the deck is completely shuffled.

--- blackJack/Form1.cs
+++ blackJack/Form1.cs
@@ -1,10 +1,30 @@
+using blackJack.classes;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
 namespace blackJack
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
-            InitializeComponent();
+            //string hello = Console.ReadLine();
+            //Console.WriteLine(hello);
+  
+            // Creates a deck
+            List<Cards> deck = Cards.CreateDeck();
+
+
+            // Shuffles all my cards
+            Cards.Shuffle(deck);
+
+
+            // Shows my cards in the console
+            foreach (var card in deck)
+            {
+                Console.WriteLine(card); 
+            }
         }
     }
 }
