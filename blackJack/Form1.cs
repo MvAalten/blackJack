8dd547dcc6a767816147f3a8c93414de4c44495f
MvAalten <157478216+MvAalten@users.noreply.github.com>
2025-03-21 13:53:31 +0100

    Added Shuffle and single card pull
    You are now able to shuffle the cards through a function in the deck.cs and you can also pull a single card through deck.cs

--- blackJack/Form1.cs
+++ blackJack/Form1.cs
@@ -1,3 +1,5 @@
+using WinFormsBlackJackApp1.classes;
+
 namespace blackJack
 {
     public partial class Form1 : Form
@@ -5,6 +7,40 @@ namespace blackJack
         public Form1()
         {
             InitializeComponent();
+            // Makes new deck
+            Deck deck = new Deck();
+
+            Console.WriteLine("Shuffling deck...");
+            deck.ShuffleCards();
+
+            while (true)
+            {
+                Console.Write("Type 'pull' to draw a card or 'exit' to quit: ");
+                // ToLower makes everything lowercase and trim prevents crashes from empty input
+                string input = Console.ReadLine()?.Trim().ToLower();
+
+                if (input == "pull")
+                {
+                    if (deck.CardsRemaining() > 0)
+                    {
+                        Card drawnCard = deck.DealCard();
+                        Console.WriteLine($"Drawn card: {drawnCard.FaceValue} of {drawnCard.Suit}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No more cards left in the deck!");
+                    }
+                }
+                else if (input == "exit")
+                {
+                    Console.WriteLine("Exiting...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command! Type 'pull' or 'exit'.");
+                }
+            }
         }
     }
 }
---
+++ blackJack/classes/deck.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using WindowsFormsApp1;
+
+namespace WinFormsBlackJackApp1.classes
+{
+    internal class Deck
+    {
+        private List<Card> cards;
+        private static Random rng = new Random();
+
+        public Deck()
+        {
+            // New list of cards
+            cards = new List<Card>();
+            // Initializes the deck
+            InitializeDeck();
+        }
+
+        private void InitializeDeck()
+        {
+            // For every suit loop
+            foreach (Suits suit in Enum.GetValues(typeof(Suits)))
+            {
+                // In every suit loop loop for every FaceValue
+                foreach (FaceValues faceValue in Enum.GetValues(typeof(FaceValues)))
+                {
+                     cards.Add(new Card(suit, faceValue));
+                }
+            }
+        }
+
+        public void ShuffleCards()
+        {
+            Shuffle();
+        }
+
+        public Card DealCard()
+        {
+            if (cards.Count == 0)
+            {
+                Console.WriteLine("No cards left");
+
+            }
+            // Goes into my public class Card in my card.cs file creates a new variable (dealtCard) and fills this with the first
+            // instance of the cards array
+            Card dealtCard = cards[0];
+            cards.RemoveAt(0);
+            return dealtCard;
+        }
+
+        public void Shuffle()
+        {
+            for (int cardCount = cards.Count - 1; cardCount > 0; cardCount--)
+            {
+                int R = rng.Next(cardCount + 1);
+                (cards[cardCount], cards[R]) = (cards[R], cards[cardCount]);
+            }
+        }
+
+        public int CardsRemaining() // debugging
+        {
+            return cards.Count;
+        }
+    }
+}
