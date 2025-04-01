83946082b41018739ad81d3224907f498439d6aa
MvAalten <157478216+MvAalten@users.noreply.github.com>
2025-04-01 13:13:43 +0200

    Base Hit function I added the base for my hit function

---
+++ blackJack/Classes/dealer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using WindowsFormsApp1;
+using WinFormsBlackJackApp1.classes;
+
+namespace WinFormsBlackJackApp1.classes
+{
+    internal class Dealer
+    {
+    }
+}
---
+++ blackJack/Classes/hand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using WindowsFormsApp1;
+
+public class Hand
+{
+    // What do i have in my hand
+    private List<Card> cards;
+    private int score;
+    private bool winner;
+    private bool loser;
+    private bool blackjack;
+    public Hand()
+    {
+        cards = new List<Card>();
+        score = 0;
+        winner = false;
+        loser = false;
+        blackjack = false;
+    }
+
+    // Adds card to hand
+    public void Hit(Card card)
+    {
+        cards.Add(card);
+    }
+}
